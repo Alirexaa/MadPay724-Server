6a35d7d877d830fd5c04acf19b9aa1e19386b115
Alirexaa <Alireza.Baloochi1380@gmail.com>
2021-03-03 19:02:23 +0330

    Implement VerifyPasswordHash

--- MadPay724.Common/Helper/Utilities.cs
+++ MadPay724.Common/Helper/Utilities.cs
@@ -8,10 +8,25 @@ namespace MadPay724.Common.Helper
     {
         public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using (var hmac= new System.Security.Cryptography.HMACSHA256())
+            using (var hmac = new System.Security.Cryptography.HMACSHA256())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA256(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i]!=passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
         }
     }
