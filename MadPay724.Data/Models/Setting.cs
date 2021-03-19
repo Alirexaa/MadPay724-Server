0741716fdcc67d4acfcec5d6958337afbc4014b2
Alirexaa <Alireza.Baloochi1380@gmail.com>
2021-03-20 02:20:55 +0330

    Add Settings table

---
+++ MadPay724.Data/Models/Setting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MadPay724.Data.Models
+{
+    public class Setting: BaseEntity<short>
+    {
+        public Setting()
+        {
+            CreatedDate = DateTime.Now;
+            ModifiedDate = DateTime.Now;
+        }
+        [Required]
+        public string CloudinaryCloudName { get; set; }
+        [Required]
+        public string CloudinaryAPIKey { get; set; }
+        [Required]
+        public string CloudinaryAPISecret { get; set; }
+    }
+}
